9b5a86b6e8e4c1644cc6dc5d9121ae3f10bbdabe
NEWUSER <duy9117@gmai.com>
2023-09-06 20:36:40 +0700

    handle server error

--- server/route/index.js
+++ server/route/index.js
@@ -1,7 +1,11 @@
 const userRouter = require("./user");
+const { notFound, errorHandler } = require("../middleware/errorHandler");
 
 const initRoutes = (app) => {
     app.use("/api/v1/user", userRouter);
+
+    app.use(notFound);
+    app.use(errorHandler);
 };
 
 module.exports = initRoutes;
